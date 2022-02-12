60d1b7ef19582cca388fe8ec1b68b9f5aa3a1c53
Usuario <Usuario@DESKTOP-BA20I58>
2022-02-12 17:36:45 +0100

    VERSION 0

--- src/demo1/demo1.java
+++ src/demo1/demo1.java
@@ -1,10 +1,5 @@
 package demo1;
 
 public class demo1 {
-// VERSION INICIAL DE LA DEMO
-	//CODIGO PARA CAMBIAR
-		//Segunda version de codigo para hacer el primer
-	//Estamo ahora en la rama demo 2 
-	//vamos a hacer meerch
+//VERSION INICIAL MOMENTANEA
 }
-//mas cambios para mirar 
